73f888bf7abb1858303d8a26038ea98e51ccf264
Gabriel Henrique Fiszczuk Brandeburski <brandeburskigabrielhenrique@gmail.com>
2022-08-02 13:13:34 -0300

    Added option to remove patients

---
+++ js/remove-paciente.js
@@ -0,0 +1,6 @@
+let tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function(event){
+	if (event.target.tagName == 'TD'){
+		event.target.parentNode.remove();
+	}
+})
